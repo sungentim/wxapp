1b2848879f6941162fa9817e15319f88e9563bac
sungen <sungentim@gmail.com>
2018-11-19 17:43:47 +0800

    init

---
+++ miniprogram/config.js
@@ -0,0 +1,7 @@
+const host = 'dsn.apizza.net/mock/baa7d8d7923f2a0c153e0b0c30051d65'
+const config = {
+  host,
+  list:  `https://${host}/blog/list.cc`,
+  detail: `https://${host}/blog/detail.cc`,
+}
+module.exports = config;
---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,152 @@
+//index.js
+const app = getApp()
+const requestUrl = require('../../config.js').list
+Page({
+  data: {
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: '',
+    tradeList:[]
+  },
+
+  onLoad: function () {
+    this.getList();
+    //uploader - containeruploader - container
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  onGetUserInfo: function(e) {
+    if (!this.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
+  },
+
+  onGetOpenid: function() {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        wx.navigateTo({
+          url: '../userConsole/userConsole',
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
+  },
+
+  // 上传图片
+  doUpload: function () {
+    // 选择图片
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+
+        wx.showLoading({
+          title: '上传中',
+        })
+
+        const filePath = res.tempFilePaths[0]
+        
+        // 上传图片
+        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
+        wx.cloud.uploadFile({
+          cloudPath,
+          filePath,
+          success: res => {
+            console.log('[上传文件] 成功：', res)
+
+            app.globalData.fileID = res.fileID
+            app.globalData.cloudPath = cloudPath
+            app.globalData.imagePath = filePath
+            
+            wx.navigateTo({
+              url: '../storageConsole/storageConsole'
+            })
+          },
+          fail: e => {
+            console.error('[上传文件] 失败：', e)
+            wx.showToast({
+              icon: 'none',
+              title: '上传失败',
+            })
+          },
+          complete: () => {
+            wx.hideLoading()
+          }
+        })
+
+      },
+      fail: e => {
+        console.error(e)
+      }
+    })
+  },
+  takePhoto:function(){
+    console.log('1111');
+    wx.createCameraContext(this).takePhoto('/temp');
+  },
+  /**
+   * 下拉刷新(请求服务获取数据并赋值)
+   */
+  getList: function () {
+    let self = this;
+    wx.request({
+      url: requestUrl,
+      success:(result)=>{
+        console.log(result);
+        if (result.data.status == 1) {
+        
+          self.setData({
+            "tradeList": result.data.data.list,
+          });
+        }
+      },
+    })
+  },
+  /**
+   * 查看商品详情
+   */
+  showTradeDetail: function (e) {
+    wx.navigateTo({
+      url: '/pages/articleDetail/articleDetail',
+    })
+  },
+})
